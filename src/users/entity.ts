dce45fae531f71bd7a69078b51c463008517a567
weichichou <weichi.cc@gmail.com>
2019-11-12 13:27:41 +0100

    add validation

--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
+import { IsString, Length, IsEmail } from 'class-validator'
 
 @Entity()
 export default class User extends BaseEntity {
@@ -7,15 +8,22 @@ export default class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id?: number
 
+  @IsString()
+  @Length(2)
   @Column('text', {nullable:false})
   firstName: string
-
+  
+  @IsString()
+  @Length(2)
   @Column('text', {nullable:false})
   lastName: string
 
+  @IsEmail()
   @Column('text', {nullable:false})
   email: string
 
+  @IsString()
+  @Length(3)
   @Column('text')
   city: string
 }
